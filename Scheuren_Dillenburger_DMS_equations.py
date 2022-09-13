792356a2211e5d40b26774728e6ad2f31411afa1
Seth Weiss <sweisss@gmail.com>
2022-09-12 19:05:30 -0700

    Added another starter example.

--- Scheuren_Dillenburger_DMS_equations.py
+++ Scheuren_Dillenburger_DMS_equations.py
@@ -5,9 +5,12 @@ Description: A program that uses the Runge Kutta method (RK4) of numerical analy
 to solve the differential equations presented in papers by H. Scheuren and M. Dillenburger.
 This program starts by lifting the RK4 example from Geeks for Geeks written by Arpit Agarwal:
 https://www.geeksforgeeks.org/runge-kutta-4th-order-method-solve-differential-equation/
+This program also used the code from the following link as a starting point:
+https://www.codesansar.com/numerical-methods/runge-kutta-fourth-order-rk4-python-program.htm
 """
 
 # Python program to implement Runge Kutta method
+# From G4G
 # A sample differential equation "dy / dx = (x - y)/2"
 def dydx(x, y):
     return ((x - y) / 2)
@@ -36,11 +39,63 @@ def rungeKutta(x0, y0, x, h):
     return y
 
 
-# Driver method
-x0 = 0
-y = 1
-x = 2
-h = 0.2
-print('The value of y at x is:', rungeKutta(x0, y, x, h))
+# RK-4 method python program
+# from codesansar.com
 
-# This code is contributed by Prateek Bhindwar
+# function to be solved
+def f(x, y):
+    return x + y
+
+
+# or
+# f = lambda x: x+y
+
+# RK-4 method
+def rk4(x0, y0, xn, n):
+    # Calculating step size
+    h = (xn - x0) / n
+
+    print('\n--------SOLUTION--------')
+    print('-------------------------')
+    print('x0\ty0\tyn')
+    print('-------------------------')
+    for i in range(n):
+        k1 = h * (f(x0, y0))
+        k2 = h * (f((x0 + h / 2), (y0 + k1 / 2)))
+        k3 = h * (f((x0 + h / 2), (y0 + k2 / 2)))
+        k4 = h * (f((x0 + h), (y0 + k3)))
+        k = (k1 + 2 * k2 + 2 * k3 + k4) / 6
+        yn = y0 + k
+        print('%.4f\t%.4f\t%.4f' % (x0, y0, yn))
+        print('-------------------------')
+        y0 = yn
+        x0 = x0 + h
+
+    print('\nAt x=%.4f, y=%.4f' % (xn, yn))
+
+
+if __name__ == "__main__":
+    # This code is contributed by Prateek Bhindwar
+    # Driver method
+    x0 = 0
+    y = 1
+    x = 2
+    h = 0.2
+    print('The value of y at x is:', rungeKutta(x0, y, x, h))
+
+
+
+    # Inputs
+    print('Enter initial conditions:')
+    x0 = float(input('x0 = '))
+    y0 = float(input('y0 = '))
+
+    print('Enter calculation point: ')
+    xn = float(input('xn = '))
+
+    print('Enter number of steps:')
+    step = int(input('Number of steps = '))
+
+    # RK4 method call
+    rk4(x0, y0, xn, step)
+
