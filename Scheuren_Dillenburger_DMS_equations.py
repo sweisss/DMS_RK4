2fab17bf3271583eac3d4cd02ab78a36e3bf5fd7
Seth Weiss <sweisss@gmail.com>
2022-10-01 21:55:51 -0700

    Implemented eqn 4 with constants as local variables. Graph looks vary confusing and changes drastically based on step size. Need to double check math and i/o.

--- Scheuren_Dillenburger_DMS_equations.py
+++ Scheuren_Dillenburger_DMS_equations.py
@@ -9,76 +9,37 @@ This program also used the code from the following link as a starting point:
 https://www.codesansar.com/numerical-methods/runge-kutta-fourth-order-rk4-python-program.htm
 """
 import math
-
-# import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 
 
-# RK-4 method python program
-# from codesansar.com
-
-# function to be solved
+###### Function to be solved ######
 # Example problem
 def f_example(x, y):
     return y - x
 
-# or
-# f = lambda x: y - x
-
-def c_i(c0, k, t0, tn, h):
-    """
-    Equation 1 Dillenburger (2017)
-    An exponential decay equation that calculates the cleavage of DMSP and
-    formation of DMS at a certain time of the boil. 
-    :param c0:  DMSP content at start of time span
-    :param k:   Rate constant
-    :param t0:  Start of time span
-    :param tn:  End of time span
-    :param h:   Time step
-    :return:    DMSP content at end of time span
-    """
-    c = []
-    for i in range(t0, tn+h, h):
-        c.append(c0 * math.exp(-k * i))
-    return c
-
-
-def dc_i(t, c_i0, k=0.00025):
-    """
-    An alternative form of eq. 1 from Dillenburger (2017), presented as eq. 10 in Scheuren (2014).
-    The right side of these two equations are equivelant. The difference is that the left side is now explicitly a
-    differential equation that can be evaluated using the RK4 method.
-    TODO make sure the parameters line up correctly with the paper.
-    TODO Review implementation of higher order functions. What values are being passed for t and c_i?
-    :param c_i: DMSP content
-    :param t: Time
-    :param k: Rate constant for chemical reaction.
-    :return: DMSP content after a period of time in the boil.
-    """
-    return k * c_i0 * math.exp(-k * t)
-
 
 # Equation 4 Dillenburger (2017)
-def f4(D_dot, L0, Ki, xi, ci, k, t):
+def f4(x_i, t):
     """
     Equation 4 from Dillenburger (2017) calculates 'the boiling of wort in a kettle through direct
     heating of tank walls in terms of the evaporation of DMS in water and teh simultaneous reproduction
     of DMSP'
-    :param D_dot:   Steam flow (a constant or a variable?)
-    :param L0:      Wort volume at point t0 (most likely a constant)
-    :param Ki:      Volatility of component i (DMS) (most likely a constant)
-    :param xi:      Dimethyl sulphide content
-    :param ci:      Precursor content of component i (DMSP)
-    :param k:       Rate constant 1/s
+    Local variables go against python convention to keep them consistent with the equation in the paper.
+    :param x_i:      Dimethyl sulphide content
     :param t:       time
     :return:        The change in DMS content dependent on a process time
     """
-    return
+    D_dot = 1.1     # Steam flow in L/s
+    L_0 = 73200     # Initial wort volume (boil start)
+    K_i = 78        # Volatility
+    k = 0.00025     # Rate constant
+    c_i0 = 227      # Dimethyl suphide precursor (DMSP) in micrograms/L
+
+    return -(D_dot/L_0) * (K_i * x_i - x_i - c_i0) + k * c_i0 * math.exp(-k * t)
 
 
-
-# RK-4 method
+###### RK-4 method ######
 def rk4(f, t0, x0, h, n):
     """
     The parameters of this RK4 method have been altered to reflect those in the Scheuren/Dillenburger papers.
@@ -120,21 +81,18 @@ if __name__ == "__main__":
     print('Enter number of steps:')
     step = int(input('Number of steps = '))
 
-    # RK4 method call dc_i
-    data = rk4(dc_i, t0, x0, h, step)
+    ###### RK4 method call f4 ######
+    data = rk4(f4, t0, x0, h, step)
     t = data["tn"]
     x = data["xn"]
 
-    # RK4 method call example function
+    ###### RK4 method call example function ######
     # data = rk4(f_example, t0, x0, h, step)
     # t = data["tn"]
     # x = data["xn"]
 
-    # plot the data
+    ###### plot the data #####
     plt.plot(t, x)
     plt.xlabel("t (in seconds)")
     plt.ylabel("x")
     plt.show()
-
-
-
